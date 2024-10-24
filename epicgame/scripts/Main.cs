98376da704187a0cf75553687b893213683f90c4
Jake <jacobatienza14@gmail.com>
2024-10-24 18:28:48 -0400

    Roblox
    dwadwadwadaw

--- epicgame/scripts/Main.cs
+++ epicgame/scripts/Main.cs
@@ -6,6 +6,10 @@ public partial class Main : Control
 	public override void _Ready()
 	{
 		GetNode<TextureButton>("VBoxContainer/start").GrabFocus();
+		var planet = GetNode<AnimatedSprite2D>("purple");
+		planet.Play("rotate");
+		var planet2 = GetNode<AnimatedSprite2D>("saturn");
+		planet2.Play("rotate");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
